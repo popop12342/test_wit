c809743b7b4a96eb7410808e744830abe392df2b
popop12342 <felipepinna17@gmail.com>
2018-02-22 08:08:42 -0300

    renomeando arquivo principal

--- my_io/IOManager.py
+++ my_io/IOManager.py
@@ -3,5 +3,7 @@ class IOManager:
     def imprime(self, text):
         print(text)
 
-    def le(self):
+    def le(self, text=None):
+        if text:
+            return input(text)
         return input()
